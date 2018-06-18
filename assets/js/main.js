2ea0a193aeb8d9401f851b7c622b806d2aa61c84
Mark Dermanov <dev@dermanov.ru>
2018-06-18 11:39:05 +0300

    select game mode and render game field

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,9 +7,56 @@
  *
  */
 
-var minesweeper = new Vue({
+var minesweeper_app = new Vue({
     el: '#minesweeper',
     data: {
-        message: 'Hello Vue.js!'
+        levels : [
+            {
+                code : 'easy',
+                size : 3,
+                title : "Легкий",
+            },
+            {
+                code : 'normal',
+                size : 6,
+                title : "Нормальный",
+            },
+            {
+                code : 'hard',
+                size : 9,
+                title : "Сложный",
+            }
+        ],
+        level : {},
+        game_started : false,
+        game_time_seconds : 0
+    },
+    computed : {
+        game_time_formated : function () {
+            var result = "";
+
+            result += Math.round(this.game_time_seconds / 3600) + "h ";
+            result += Math.round(this.game_time_seconds % 3600 / 60) + "m ";
+            result += Math.round(this.game_time_seconds % 60) + "s ";
+
+            return result;
+        }
+    }, mounted : function() {
+
+    },
+    methods : {
+        start_game : function () {
+            if (!this.level.code){
+                alert("Нужно выбрать уровень!");
+                return;
+            }
+
+            this.game_started = true;
+
+            // start game timer
+            setInterval(function () {
+                minesweeper_app.game_time_seconds++;
+            }, 1000)
+        }
     }
 });
--- index.php
+++ index.php
@@ -17,28 +17,42 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     
     <link rel="stylesheet" href="/assets/css/main.css?v=<?=filemtime($_SERVER["DOCUMENT_ROOT"] . "/assets/css/main.css")?>" />
-    <script src="/assets/js/main.js?v=<?=filemtime($_SERVER["DOCUMENT_ROOT"] . "/assets/js/main.js")?>"></script>
     <script src="https://unpkg.com/vue"></script>
 </head>
 <body>
     <div id="minesweeper" class="minesweeper">
-        <div class="table">
-            <div class="row">
-                <div class="cell default"></div>
-                <div class="cell default"></div>
-                <div class="cell default"></div>
-            </div>
-            <div class="row">
-                <div class="cell flag"></div>
-                <div class="cell mine"></div>
-                <div class="cell default"></div>
-            </div>
-            <div class="row">
-                <div class="cell open"></div>
-                <div class="cell open"></div>
-                <div class="cell default"></div>
+        
+        <div v-if="!game_started">
+            <p>Выберите уровень
+                <select name="" id="" v-model="level" placeholder="- Выбрать уровень -">
+                    <option v-for="(level, index) in levels" v-bind:value="level" >
+                        {{ level.title }}
+                    </option>
+                </select>
+            </p>
+            
+            <p>
+                <button @click="start_game">Начать игру</button>
+            </p>
+        </div>
+    
+        <div v-if="game_started">
+            <p>Режим игры: {{level.title}} </p>
+            <p>Время игры: {{game_time_formated}} </p>
+    
+    
+            <div class="table">
+                <template v-for="item in level.size">
+                    <div class="row">
+                        <div v-for="item1 in level.size" class="cell default"></div>
+                    </div>
+                </template>
             </div>
         </div>
+    
+        
     </div>
+
+    <script src="/assets/js/main.js?v=<?=filemtime($_SERVER["DOCUMENT_ROOT"] . "/assets/js/main.js")?>"></script>
 </body>
 </html>
